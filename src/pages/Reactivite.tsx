a3fdefb9834aec148a1b2fba366520495692add1
thais <thais.r@live.fr>
2025-06-17 16:49:27 +0200

    Router + reactivité

--- src/pages/Reactivite.tsx
+++ src/pages/Reactivite.tsx
@@ -1,13 +1,16 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const Reactivite = () => {
 
     let count = 0;
 
-    const stateArray = useState('Hello World');
-    const state = stateArray[0];
-    const setState = stateArray[1];
-    let [counterDynamic, setCounterDynamic] = useState<number>(0);
+    //const stateArray = useState('Hello World');
+   // const state = stateArray[0];
+   // const setState = stateArray[1];
+    const [counterDynamic, setCounterDynamic] = useState<number>(0);
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [books, setBooks] = useState<string[]>([]);
+    const newBook = useRef<HTMLInputElement | null>(null);
 
     const increment = () => {
         count++;
@@ -31,6 +34,41 @@ const Reactivite = () => {
         setCounterDynamic(previous => previous - 1);
     }
 
+    const login = () => {
+        setIsLoggedIn(true);
+    }
+
+    const logout = () => {
+        setIsLoggedIn(false);
+    }
+
+    const LoggedInBlock = () => (
+        <div>
+            <h3>Bonjour Jean Michel</h3>
+            <p>
+                <button onClick={logout}>Logout</button>
+            </p>
+        </div>
+    );
+
+    const LogoutBlock = () => (
+        <div>
+            <h3>Vous n'êtes pas connecté•e</h3>
+            <p>
+                <button onClick={login}>Login</button>
+            </p>
+        </div>
+    )
+
+    const addBook = () => {
+        console.log(newBook.current?.value);
+        if(newBook.current?.value) {
+            const value = newBook.current?.value;
+            setBooks(prev => [...prev, value]);
+            newBook.current.value = '';
+        }
+    }
+
 
     return (
         <>
@@ -52,6 +90,25 @@ const Reactivite = () => {
                     <button onClick={incrementDynamic}>+</button>
                 </div>
 
+            </section>
+
+            <section>
+                <h2>Login / Logout</h2>
+
+                {isLoggedIn ? <LoggedInBlock /> : <LogoutBlock />}
+            </section>
+
+            <section>
+                <h2>Mes livres</h2>
+
+                <p>
+                    <input ref={newBook}/>
+                    <button onClick={addBook}>Add</button>
+                </p>
+
+                <ul>
+                    {books.map((book) => (<li key={book}>{book}</li> ))}
+                </ul>
             </section>
         </>
     )
