e8634b6b963885d6a2a009e94363fe8b5018bc13
thais <thais.r@live.fr>
2025-06-18 12:17:35 +0200

    UseEffect

--- src/pages/Hooks/UseEffect.tsx
+++ src/pages/Hooks/UseEffect.tsx
@@ -1,8 +1,25 @@
+import {useState} from "react";
+
 const UseEffect = () => {
+    console.log('Lancement de la fonction composant');
+
+    const [counter, setCounter] = useState(0);
+    let staticCounter = 0;
+
+    const incrementStatic = () => {
+        staticCounter++;
+    }
 
     return (
         <>
             <h1>Use Effect</h1>
+
+            <p>
+                <button onClick={() => setCounter(p => p + 1)} >Reactif : {counter}</button>
+            </p>
+            <p>
+                <button onClick={incrementStatic}>Static: {staticCounter}</button>
+            </p>
         </>
     )
 }
