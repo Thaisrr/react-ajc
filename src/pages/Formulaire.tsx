a2271b8ae9cf1dacb8e6428fee81b0009eb6f37f
thais <thais.r@live.fr>
2025-06-19 11:43:48 +0200

    Formulaire

--- src/pages/Formulaire.tsx
+++ src/pages/Formulaire.tsx
@@ -3,7 +3,9 @@ import type {UserType} from "../utils/types/UserType.ts";
 
 const Formulaire = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm<UserType>();
+    const {register, handleSubmit, formState: {errors}, watch, getValues} = useForm<UserType>();
+
+    const watchPassword = watch('password');
 
     const save = (user: UserType) => {
         console.log(user);
@@ -28,6 +30,62 @@ const Formulaire = () => {
                     })}/>
                     <p>{errors.email?.message}</p>
                 </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' id='password' {...register('password', {
+                        validate: (value: string) => (value === 'password') || 'Le mot de passe doit être password'
+                    })}/>
+                    <p>{errors.password?.message}</p>
+                    {watchPassword === 'password' && <p>Bravo ! </p>}
+                </div>
+                <div>
+                    <label htmlFor='validatePassword'>Validate Password</label>
+                    <input type='password' id='validatePassword' {...register('validatePassword', {
+                        validate: (value: string) => (value === watchPassword) || 'Ne correspond pas au mot de passe.'
+                    })} />
+                    <p>{errors.validatePassword?.message}</p>
+                </div>
+                <div>
+                    <label htmlFor='title' {...register('title')}>Titre</label>
+                    <select id='title'>
+                        <option value="empress">Impératrice</option>
+                        <option value="jedi">Jedi</option>
+                    </select>
+                </div>
+                <fieldset>
+                    <legend>Animal préféré</legend>
+
+                    <div>
+                        <label>Requin Marteau
+                            <input type='radio' value='shark' {...register('favoriteAnimal')}/>
+                        </label>
+                        <label>Mante Religieuse
+                            <input type='radio' value='mantis' {...register('favoriteAnimal')}/>
+                        </label>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Film préféré</legend>
+
+                    <div>
+                        <label htmlFor='movieTitle'>Titre</label>
+                        <input id='movieTitle' {...register('favoriteMovie.title', {
+                            validate: (value: string) => {
+                                if(getValues("favoriteMovie.synopsis") && !value) {
+                                    return "Doit être renseigné si il y a un synopsis !"
+                                }
+                                return true
+                            }
+                        })}/>
+                        <p>{errors.favoriteMovie?.title?.message}</p>
+                    </div>
+                    <div>
+                        <label htmlFor='synopsis'>Synopsis</label>
+                        <textarea id='synopsis' {...register('favoriteMovie.synopsis')}></textarea>
+                    </div>
+                </fieldset>
+
                 <button type='submit'>Submit</button>
             </form>
         </>
--- src/utils/types/UserType.ts
+++ src/utils/types/UserType.ts
@@ -1,4 +1,14 @@
 export type UserType = {
     username: string;
     email: string;
+    password: string;
+    validatePassword: string;
+    title: string;
+    favoriteAnimal: string;
+    favoriteMovie: MovieType;
+}
+
+type MovieType = {
+    title: string;
+    synopsis: string;
 }
